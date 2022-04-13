052fc8bcfcaa05567c5135467fa370114d6880c1
federico-fiou-cogne <federico.fiou@cogne.com>
2022-04-13 09:30:11 +0000

    Aggiunti commenti

--- boxwear.py
+++ boxwear.py
@@ -6,16 +6,19 @@ plt.style.use('seaborn')
 
 filename = '/data/metalm/tool_wear_304.txt'
 
+#leggiamo le colonne separate da |
 df = pd.read_csv(filename,sep='|')
-df = df.drop(df[df.materiale=='F304LH'].index)
+#togliamo LH dai dati
+df = df.drop(df[df.materiale=='F304LH'].index)  
 
+#boxplot materiale
 myplot = df.boxplot(column='time',by='materiale')
 myplot.set_ylabel('tempo limite usura [min]')
 myplot.set_title('')
 myplot.set_xlabel('')
 plt.savefig('/data/metalm/output/bwear_mat.png')
 
-
+#boxplot materiale e scheda
 myplot = df.boxplot(column='time',by=['materiale','sk'],rot=90)
 myplot.set_ylabel('tempo limite usura [min]')
 myplot.set_title('')
