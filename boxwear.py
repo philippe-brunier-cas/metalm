077a2f45699e32598a61f8c62710eab8d02d34fe
federico-fiou-cogne <federico.fiou@cogne.com>
2022-04-22 07:42:34 +0000

    aggiornati nomi corretti database tool_wear

--- boxwear.py
+++ boxwear.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 plt.style.use('seaborn')
 
-filename = '/data/metalm/tool_wear_304.txt'
+filename = '/data/metalm/tool_wear.txt'
 
 #leggiamo le colonne separate da |
 df = pd.read_csv(filename,sep='|')
@@ -12,14 +12,14 @@ df = pd.read_csv(filename,sep='|')
 df = df.drop(df[df.materiale=='F304LH'].index)  
 
 #boxplot materiale
-myplot = df.boxplot(column='time',by='materiale')
+myplot = df.boxplot(column='wear_time',by='materiale')
 myplot.set_ylabel('tempo limite usura [min]')
 myplot.set_title('')
 myplot.set_xlabel('')
 plt.savefig('/data/metalm/output/bwear_mat.png')
 
 #boxplot materiale e scheda
-myplot = df.boxplot(column='time',by=['materiale','sk'],rot=90, fontsize=10, figsize=(10, 10))
+myplot = df.boxplot(column='wear_time',by=['materiale','sk'],rot=90, fontsize=10, figsize=(10, 10))
 myplot.set_ylabel('tempo limite usura [min]')
 myplot.set_title('')
 myplot.set_xlabel('')
