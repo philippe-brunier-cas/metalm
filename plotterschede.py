f77d5695c42d30ab467791516684d3e770d49b58
federico-fiou-cogne <federico.fiou@cogne.com>
2022-04-28 07:22:18 +0000

    modificate due cose

--- plotterschede.py
+++ plotterschede.py
@@ -10,14 +10,15 @@ dati = pd.read_csv('/data/metalm/database_opt.csv',sep=';')
 dati = dati.drop(dati[dati.materiale=='F304LH\n'].index) 
 
 # Creazione dataframe colori materiali
-colori_materiali =  pd.DataFrame ([['F304L1\n','b'],['F304LO\n','g'],['F304LX\n','r']],
-                    columns=['materiale','colore'])
+colori_materiali =  pd.DataFrame ([['b'],['g'],['r']],
+                    index=['F304L1\n','F304LO\n','F304LX\n'],
+                    columns=['colore'])
 
 materiali = np.unique(dati.materiale)
 
-prove = ['Speed variabile','Feed variabile', 'DOC variabile']
+#prove = ['Speed variabile','Feed variabile', 'DOC variabile']
 
-#prove = ['speed [m/min]', 'feed [mm/rev]', 'DOC [mm]']
+prove = ['speed [m/min]', 'feed [mm/rev]', 'DOC [mm]']
 
 schede = np.unique(dati.sk)
 
@@ -36,5 +37,6 @@ for prova in prove:
       df_scheda = df_materiale[df_materiale.sk == scheda]
       medie_sk = df_scheda.groupby('passata').mean()
       sdt_sk = df_scheda.groupby('passata').std()
-      plt.plot(x,y)
+      
+      plt.plot(medie_sk[prova],medie_sk['Kn [N/mm2]'],'-v',label=scheda,color=colori_materiali[] )
       
